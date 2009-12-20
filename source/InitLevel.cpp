ab51384a8ef6478c482cf7c25e8361c416ccb34e
greatflash <greatflash@ntlworld.com>
2009-12-20 21:01:29 +0000

    Hmmm... you can jump off platform edges?

--- source/InitLevel.cpp
+++ source/InitLevel.cpp
@@ -61,55 +61,38 @@ void initLevel()
 	g_platformArray[0] = new Poly();
 	g_platformArray[0]->PolyDef = new b2PolyDef();	// left
 	g_platformArray[0]->PolyDef->vertexCount = 7;
-	g_platformArray[0]->PolyDef->vertices[0].Set(-14.0f, 2.0f);
+	g_platformArray[0]->PolyDef->vertices[0].Set(-10.0f, 2.0f);
 	g_platformArray[0]->PolyDef->vertices[1].Set(-16.0f, 1.0f);
 	g_platformArray[0]->PolyDef->vertices[2].Set(-16.0f, -1.0f);
 	g_platformArray[0]->PolyDef->vertices[3].Set(-14.0f, -2.5f);
-	g_platformArray[0]->PolyDef->vertices[4].Set( 4.1f, -2.5f);
-	g_platformArray[0]->PolyDef->vertices[5].Set( 4.0f, 1.0f);
-	g_platformArray[0]->PolyDef->vertices[6].Set( 3.0f, 2.2f);
+	g_platformArray[0]->PolyDef->vertices[4].Set( 25.0f, -2.5f);
+	g_platformArray[0]->PolyDef->vertices[5].Set( 25.0f, 3.3f);
+	g_platformArray[0]->PolyDef->vertices[6].Set( 10.0f, 2.8f);
 
 	g_platformArray[0]->PolyDef->density = 0.0f;
 	g_platformArray[0]->PolyDef->friction = 1.0f;
 	g_platformArray[0]->BodyDef = new b2BodyDef();
-	g_platformArray[0]->BodyDef->position.Set(14.0f, -2.4f);
+	g_platformArray[0]->BodyDef->position.Set(15.0f, -2.4f);
 	g_platformArray[0]->BodyDef->AddShape(g_platformArray[0]->PolyDef);
 	g_platformArray[0]->Body = g_world->CreateBody(g_platformArray[0]->BodyDef);
 	
-	g_platformArray[1] = new Poly();
-	g_platformArray[1]->PolyDef = new b2PolyDef();	// pit base
-	g_platformArray[1]->PolyDef->vertexCount = 5;
-	g_platformArray[1]->PolyDef->vertices[0].Set( -10.0f, 0.0f);
-	g_platformArray[1]->PolyDef->vertices[1].Set( -10.0f, -4.5f);
-	g_platformArray[1]->PolyDef->vertices[2].Set( -8.0f, -5.8f);
-	g_platformArray[1]->PolyDef->vertices[3].Set( 4.0f, -5.7f);
-	g_platformArray[1]->PolyDef->vertices[4].Set( 4.0f, -4.0f);
-	g_platformArray[1]->PolyDef->vertices[5].Set( -8.0f, -3.0f); // WHY DOES THIS CRASH IT
-//	g_platformArray[1]->PolyDef->vertices[6].Set( 3.0f, 2.2f);
-
-	g_platformArray[1]->PolyDef->density = 0.0f;
-	g_platformArray[1]->PolyDef->friction = 1.0f;
-	g_platformArray[1]->BodyDef = new b2BodyDef();
-	g_platformArray[1]->BodyDef->position.Set(24.0f, -4.4f);
-	g_platformArray[1]->BodyDef->AddShape(g_platformArray[1]->PolyDef);
-	g_platformArray[1]->Body = g_world->CreateBody(g_platformArray[1]->BodyDef);
 	
 	g_platformArray[2] = new Poly();
 	g_platformArray[2]->PolyDef = new b2PolyDef();	// little plat
 	g_platformArray[2]->PolyDef->vertexCount = 8;
-	g_platformArray[2]->PolyDef->vertices[0].Set( -8.0f, 0.0f);
+	g_platformArray[2]->PolyDef->vertices[0].Set( -9.0f, 0.0f);
 	g_platformArray[2]->PolyDef->vertices[1].Set( -12.0f, -1.0f);
 	g_platformArray[2]->PolyDef->vertices[2].Set( -12.0f, -3.0f);
-	g_platformArray[2]->PolyDef->vertices[3].Set( -8.0f, -5.0f);	
-	g_platformArray[2]->PolyDef->vertices[4].Set( -3.0f, -5.0f);	
-	g_platformArray[2]->PolyDef->vertices[5].Set( 1.0f, -3.0f);
-	g_platformArray[2]->PolyDef->vertices[6].Set( 1.0f, -1.0f);	
+	g_platformArray[2]->PolyDef->vertices[3].Set( -8.0f, -4.0f);	
+	g_platformArray[2]->PolyDef->vertices[4].Set( -2.0f, -4.0f);	
+	g_platformArray[2]->PolyDef->vertices[5].Set( 3.0f, -2.0f);
+	g_platformArray[2]->PolyDef->vertices[6].Set( 2.0f, -0.1f);	
 	g_platformArray[2]->PolyDef->vertices[7].Set( -3.0f, 0.0f);
 
 	g_platformArray[2]->PolyDef->density = 0.0f;
 	g_platformArray[2]->PolyDef->friction = 1.0f;
 	g_platformArray[2]->BodyDef = new b2BodyDef();
-	g_platformArray[2]->BodyDef->position.Set(19.0f, -19.0f);
+	g_platformArray[2]->BodyDef->position.Set(11.0f, -21.0f);
 	g_platformArray[2]->BodyDef->AddShape(g_platformArray[2]->PolyDef);
 	g_platformArray[2]->Body = g_world->CreateBody(g_platformArray[2]->BodyDef);
 
@@ -123,7 +106,7 @@ void initLevel()
 	g_platformArray[3]->PolyDef->density = 0.0f;
 	g_platformArray[3]->PolyDef->friction = 1.0f;
 	g_platformArray[3]->BodyDef = new b2BodyDef();
-	g_platformArray[3]->BodyDef->position.Set(30.0f, 8.0f);
+	g_platformArray[3]->BodyDef->position.Set(30.0f, -15.0f);
 	g_platformArray[3]->BodyDef->AddShape(g_platformArray[3]->PolyDef);
 	g_platformArray[3]->Body = g_world->CreateBody(g_platformArray[3]->BodyDef);
 	
@@ -137,21 +120,21 @@ void initLevel()
 	g_platformArray[4]->PolyDef->density = 0.0f;
 	g_platformArray[4]->PolyDef->friction = 1.0f;
 	g_platformArray[4]->BodyDef = new b2BodyDef();
-	g_platformArray[4]->BodyDef->position.Set(30.0f, 8.0f);
+	g_platformArray[4]->BodyDef->position.Set(30.0f, -15.0f);
 	g_platformArray[4]->BodyDef->AddShape(g_platformArray[4]->PolyDef);
 	g_platformArray[4]->Body = g_world->CreateBody(g_platformArray[4]->BodyDef);
 	
 	g_platformArray[5] = new Poly();
 	g_platformArray[5]->PolyDef = new b2PolyDef();	// plat under bongo
 	g_platformArray[5]->PolyDef->vertexCount = 4;
-	g_platformArray[5]->PolyDef->vertices[0].Set( 0.0f, 0.0f);
-	g_platformArray[5]->PolyDef->vertices[1].Set( 0.0f, -12.0f);
-	g_platformArray[5]->PolyDef->vertices[2].Set( 13.0f,-12.0f);
-	g_platformArray[5]->PolyDef->vertices[3].Set( 13.0f, 0.0f);	
+	g_platformArray[5]->PolyDef->vertices[0].Set( 0.0f, -0.5f);
+	g_platformArray[5]->PolyDef->vertices[1].Set( 0.0f, -4.0f);
+	g_platformArray[5]->PolyDef->vertices[2].Set( 13.0f,-4.0f);
+	g_platformArray[5]->PolyDef->vertices[3].Set( 13.0f, 1.0f);	
 	g_platformArray[5]->PolyDef->density = 0.0f;
 	g_platformArray[5]->PolyDef->friction = 1.0f;
 	g_platformArray[5]->BodyDef = new b2BodyDef();
-	g_platformArray[5]->BodyDef->position.Set(27.0f, 2.0f);
+	g_platformArray[5]->BodyDef->position.Set(27.0f, -20.5f);
 	g_platformArray[5]->BodyDef->AddShape(g_platformArray[5]->PolyDef);
 	g_platformArray[5]->Body = g_world->CreateBody(g_platformArray[5]->BodyDef);
 }
