95703ab0273deff2bea7ac9507c06d46f13f9bad
greatflash <greatflash@ntlworld.com>
2010-01-04 13:07:54 +0000

    Tidy

--- source/SpecialFX.cpp
+++ source/SpecialFX.cpp
@@ -42,19 +42,12 @@ void drawTrail()
 
 	for (int register i=0; i<TRAILAMOUNT; i++)
 	{
-		// Set the alpha value so it will alpha blend out more towards the end of the trail
-		//int alphaValue = (((float)(TRAILAMOUNT - i) / (float) TRAILAMOUNT) * 30.0F) + 1;		// just trying to take the calcs out of the loop
-		//glPolyFmt(POLY_ALPHA(alphaValue) | POLY_CULL_BACK | POLY_FORMAT_LIGHT0 | POLY_FORMAT_LIGHT1 | POLY_FORMAT_LIGHT2 | POLY_FORMAT_LIGHT3);
-
 		glPolyFmt(POLY_ALPHA((alphaValue -= alphaStep)) | POLY_CULL_BACK | POLY_FORMAT_LIGHT0 | POLY_FORMAT_LIGHT1 | POLY_FORMAT_LIGHT2 | POLY_FORMAT_LIGHT3);
 	
 		glBegin(GL_QUAD);
 		
 		drawPos += TRAILINTERVAL;  // - (i / TRAILINTERVAL);	// the (i/ti) is to pull the end sections closer (a kludge)
 		if (drawPos > (TRAILINTERVAL * TRAILAMOUNT) - 1) drawPos = drawPos - (TRAILINTERVAL * TRAILAMOUNT);
-		
-	//	double n1 = sqrt(x1 * x1 + y1 * y1), n2 = sqrt(TrailPoints[drawPos].X * TrailPoints[drawPos].X + TrailPoints[drawPos].Y * TrailPoints[drawPos].Y);
-	//	double a1 = acos((x1 * TrailPoints[drawPos].X + y1 * TrailPoints[drawPos].Y)/(n1 * n2)) * 180 / PI;	
 		
 		double a1 = 180-atan2(TrailPoints[drawPos].X - x1,TrailPoints[drawPos].Y - y1) * 180 / PI; 
 		
